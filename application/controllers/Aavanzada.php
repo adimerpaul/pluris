9f25caa1cd46823592ae6da2b6ee9c78c524691b
adimerpaul <adimer101@email.com>
2019-10-01 21:12:18 -0400

    cambiados nombres

--- application/controllers/Aavanzada.php
+++ application/controllers/Aavanzada.php
@@ -2,7 +2,7 @@
 class Aavanzada extends CI_Controller{
     function index(){
         $this->load->view('templates/header');
-        $this->load->view('Aavanzada');
+        $this->load->view('aavanzada');
         $this->load->view('templates/footer');
     }
 }
